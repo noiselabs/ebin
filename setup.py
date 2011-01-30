7cc7eb11962f76a9c78d5cd0be0182da7f1dd16c
noisebleed <noisebleed@noiselabs.org>
2011-01-30 02:19:47 +0000

    Create project layout

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='ebin',
+    version=version,
+    description="A Portage-based tool to make the installation of binary packages in Gentoo Linux easy and reliable",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='emerge portage gentoo linux package manager binary binhost',
+    author='Vitor Brand\xc3\xa3o (noisebleed)',
+    author_email='noisebleed@noiselabs.org',
+    url='http://ebin.noiselabs.org',
+    license='GNU General Public License (GPL)',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['org', 'org.noiselabs'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['ebin=org.noiselabs.ebin:main']
+    }
+)
